36c941840ec049ba186eec03ba85d0da96d4bcd5
patrick-danford <pdanford@hotmail.com>
2017-03-06 18:00:58 -0800

    Initial commit

---
+++ ex1.py
@@ -0,0 +1,41 @@
+# ex1
+print "Hello World!"
+print "Hello Again!"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+# print "Another Line."
+
+print "\n\n"
+# ex2
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored in python.
+
+print "I could have code like this."  # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out a piece of code:
+# print "this won't run"
+
+print "This will run."
+
+print "\n\n"
+# ex3
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count my eggs:"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print "Is that true that 3+2 < 5-7?"
+print 3 + 2 < 5 - 7
+
+print "What is 3+2?", 3 + 2
+print "What is 5-7?", 5 - 7
+print "Oh, that's why it's false.."
+print "How about some more."
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
