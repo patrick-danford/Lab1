83f8d002bc5bc900309c184aefc71a5f584f0641
patrick-danford <pdanford@hotmail.com>
2017-03-19 19:57:52 -0700

    3-19 update

--- ex39.py
+++ ex39.py
@@ -34,3 +34,26 @@ print "Michigan's abbreviation is: ", states['michigan']
 print "Florida's abbreviation is: ", states['florida']
 
 print '-' * 10
+print "Michigan has: ", cities[states['michigan']]
+print "Florida has: ", cities[states['florida']]
+
+print '-' * 10
+for state, abbrev in states.items():
+    print "%s is abbreviated %s" % (state, abbrev)
+
+print '-' * 10
+for abbrev, city in cities.items():
+    print "%s has the city %s" % (abbrev, city)
+
+print '-' * 10
+for state, abbrev in states.items():
+    print "%s state is abbreviated %s and has the city %s" % (state, abbrev, cities[abbrev])
+
+print '-' * 10
+state = states.get('Texas')
+
+if not state:
+    print "Sorry no Texas"
+
+city = cities.get('TX', "Does not exist")
+print "The city for the state 'TX' is: %s" % city
